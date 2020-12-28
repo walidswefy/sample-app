bebf86c2bfb7ec3dde24e9d3dfbcaa08dca1d55d
Walid Sewaify <walidswefy@hotmail.com>
2020-12-28 13:15:25 +0400

    add FilterRegistrationBean to configuration

--- src/main/java/com/example/sampleapp/config/WebConfig.java
+++ src/main/java/com/example/sampleapp/config/WebConfig.java
@@ -1,7 +1,10 @@
 package com.example.sampleapp.config;
 
 import com.example.sampleapp.logging.LoggingInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +14,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    /**
+     * Bean for overriding remote host (if request comes through a proxy)
+     */
+    @Bean
+    FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new ForwardedHeaderFilter());
+        return bean;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggingInterceptor());
